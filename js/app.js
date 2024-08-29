6fcdb472acd198c61efbfd6cb0898f525871583a
Dize dev <axiozweb2022@gmail.com>
2024-08-28 21:35:55 -0500

    se termino el codigo

--- js/app.js
+++ js/app.js
@@ -21,10 +21,31 @@ window.addEventListener('DOMContentLoaded', () => {
 	verticalScrollbar.track.xAxis.element.remove();
 
 	verticalScrollbar.addListener(({ offset }) => {
-		offset.y > 0
+		const itemFull = selectAll('.item-full');
+
+		offset.y > 50
 			? gsap.to(heading, { opacity: 0 })
 			: gsap.to(heading, { opacity: 1 });
+
+		gsap.to(itemFull, { y: -offset.y, duration: 0 });
 	});
+
+	const setPositionCalculation = () => {
+		const containerFull = select('.item-container-full');
+		const containerTrasparent = select('.item-container-transparent');
+		const scrollContent = select('.scroll-content');
+
+		const scrollContenHeight = DATA.length * (ITEM_SIZE + PADDING);
+		const multiplyTime = window.innerHeight / (ITEM_SIZE + PADDING) - 2;
+
+		gsap.set(scrollContent, {
+			blockSize: scrollContenHeight + ITEM_SIZE * multiplyTime + ITEM_SIZE,
+		});
+		gsap.set(containerFull, { top: ITEM_SIZE * multiplyTime });
+		gsap.set(containerTrasparent, { y: ITEM_SIZE * multiplyTime });
+	};
+
+	window.addEventListener('resize', setPositionCalculation());
 
 	const generateList = () => {
 		const scrollContent = select('.scroll-content');
@@ -51,6 +72,7 @@ window.addEventListener('DOMContentLoaded', () => {
 			containerFull.appendChild(createItem(item, false));
 			containerTrasparent.appendChild(createItem(item, true));
 		});
+		content.appendChild(picture);
 	};
 	const createItem = (item, isTransparent = false) => {
 		const itemList = item.title.split(' ');
@@ -71,5 +93,39 @@ window.addEventListener('DOMContentLoaded', () => {
 		return itemContainer;
 	};
 
+	const iniObserver = () => {
+		const picturesList = selectAll('.img-select');
+
+		let options = {
+			root: verticalScrollbar.containerEl,
+			rootMargin: ` ${ITEM_SIZE}px 0px -${ITEM_SIZE}px 0px `,
+			threshold: 0.4,
+		};
+
+		let observer = new IntersectionObserver((entries, _) => {
+			entries.forEach(entry => {
+				entry.isIntersecting
+					? animate(entry, picturesList, 'in')
+					: animate(entry, picturesList, 'out');
+			});
+		}, options);
+
+		verticalScrollbar.containerEl
+			.querySelectorAll('.item-transparent')
+			.forEach(item => observer.observe(item));
+	};
+
+	const animate = (entry, pictireList, dir) => {
+		const target = pictireList[entry.target.dataset.id - 1];
+
+		gsap.to(target, {
+			translateY: dir === 'in' ? 0 : 500,
+			scale: dir === 'in' ? 1 : 1.5,
+			duration: 0.5,
+		});
+	};
+
 	generateList();
+	setPositionCalculation();
+	iniObserver();
 });
